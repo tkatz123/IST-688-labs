43fe3136a8527faa84249843ddc0a2db44ef6120
tkatz123 <tylerkatz110@gmail.com>
2026-09-01 11:09:30 -0400

    Added summarization option on sidebar for lab 2.

--- labs/lab2.py
+++ labs/lab2.py
@@ -8,53 +8,53 @@ st.write(
     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+#Get OPENAI API Key from secrets file
+openai_api_key = st.secrets.OPENAI_API_KEY
 
-    try:
-        # Create an OpenAI client.
-        client = OpenAI(api_key=openai_api_key)
+try:
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
 
-        client.models.list()
-        
-    except AuthenticationError:
-        st.info("🚨 Invalid OpenAI API Key")
-        st.stop()
+    client.models.list()
 
-    # Let the user upload a file via `st.file_uploader`.
-    uploaded_file = st.file_uploader(
-        "Upload a document (.txt or .md)", type=("txt", "md")
+except AuthenticationError:
+    st.info("🚨 Invalid OpenAI API Key")
+    st.stop()
+
+summarize_option = st.sidebar.selectbox(
+    'How would you like your document to be summarized?',
+    ('100 words', '2 connecting paragraphs', '5 bullet points')
+)
+
+# Let the user upload a file via `st.file_uploader`.
+uploaded_file = st.file_uploader(
+    "Upload a document (.txt or .md)", type=("txt", "md")
+)
+
+# Ask the user for a question via `st.text_area`.
+question = st.text_area(
+    "Now ask a question about the document!",
+    placeholder="Can you give me a short summary?",
+    disabled=not uploaded_file,
+)
+
+if uploaded_file and question:
+
+    # Process the uploaded file and question.
+    document = uploaded_file.read().decode()
+    messages = [
+        {
+            "role": "user",
+            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+        }
+    ]
+
+    # Generate an answer using the OpenAI API.
+    stream = client.chat.completions.create(
+        model="gpt-5-nano",
+        messages=messages,
+        stream=True,
     )
 
-    # Ask the user for a question via `st.text_area`.
-    question = st.text_area(
-        "Now ask a question about the document!",
-        placeholder="Can you give me a short summary?",
-        disabled=not uploaded_file,
-    )
-
-    if uploaded_file and question:
-
-        # Process the uploaded file and question.
-        document = uploaded_file.read().decode()
-        messages = [
-            {
-                "role": "user",
-                "content": f"Here's a document: {document} \n\n---\n\n {question}",
-            }
-        ]
-
-        # Generate an answer using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-5-nano",
-            messages=messages,
-            stream=True,
-        )
-
-        # Stream the response to the app using `st.write_stream`.
-        st.write_stream(stream)
+    # Stream the response to the app using `st.write_stream`.
+    st.write_stream(stream)
