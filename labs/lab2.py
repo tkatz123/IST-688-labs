d57af8d48bd3199e08dab50d98eae843d738a63e
tkatz123 <tylerkatz110@gmail.com>
2026-09-01 11:31:51 -0400

    Completed lab 2.

--- labs/lab2.py
+++ labs/lab2.py
@@ -21,37 +21,45 @@ except AuthenticationError:
     st.info("🚨 Invalid OpenAI API Key")
     st.stop()
 
+#Ask the user how they want their document summarized
 summarize_option = st.sidebar.selectbox(
     'How would you like your document to be summarized?',
     ('100 words', '2 connecting paragraphs', '5 bullet points')
 )
+
+#Define a prompt for summarization based on option selected above
+if summarize_option == '100 words':
+    summarize_prompt = 'Summarize this document in 100 words or less. NEVER exceed the 100 word summary limit.'
+elif summarize_option == '2 connecting paragraphs':
+    summarize_prompt = 'Summarize this document in two individual connecting paragraphs. NEVER exceed the 2 paragraph limit. MAKE SURE the paragraphs flow into one another.'
+elif summarize_option == '5 bullet points':
+    summarize_prompt = 'Summarize this document in 5 individual bullet points. NEVER exceed the five bullet point limit.'
+
+#Let the user select which model they want to use
+if st.sidebar.checkbox('Use advanced model'):
+    model = 'gpt-5.4-mini'
+else:
+    model = 'gpt-5.4-nano'
 
 # Let the user upload a file via `st.file_uploader`.
 uploaded_file = st.file_uploader(
     "Upload a document (.txt or .md)", type=("txt", "md")
 )
 
-# Ask the user for a question via `st.text_area`.
-question = st.text_area(
-    "Now ask a question about the document!",
-    placeholder="Can you give me a short summary?",
-    disabled=not uploaded_file,
-)
-
-if uploaded_file and question:
+if uploaded_file:
 
     # Process the uploaded file and question.
     document = uploaded_file.read().decode()
     messages = [
         {
             "role": "user",
-            "content": f"Here's a document: {document} \n\n---\n\n {question}",
+            "content": f"Here's a document: {document} \n\n---\n\n {summarize_prompt}",
         }
     ]
 
     # Generate an answer using the OpenAI API.
     stream = client.chat.completions.create(
-        model="gpt-5-nano",
+        model=model,
         messages=messages,
         stream=True,
     )
